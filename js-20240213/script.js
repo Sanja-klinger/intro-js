a1cc09050449a7b8a09c7464d292ab85da42bb40
Sanja Klinger <sladjana.klinger@gmail.com>
2024-02-15 11:18:34 +0100

    feat: typing code for practicing console.log

--- js-20240213/script.js
+++ js-20240213/script.js
@@ -1,3 +1,9 @@
 console.log("Dje si, Djenis!");
 console.log("Nidje veze");
 console.log('Hallo "Welt"');
+console.log(255 / 5);
+console.log(1 > 1);
+console.log(1 >= 1);
+console.log("1" === 1);
+console.log("1" !== 2);
+console.log("1" + 1);
