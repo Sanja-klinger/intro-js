7fec28c715d4b2ed5686ec47c82fb842a9ff98ea
Sanja Klinger <sladjana.klinger@gmail.com>
2024-02-19 09:25:55 +0100

    feat: creat folder js-2024 with files index.html and script.js, include js in html

--- js-20240215/script.js
+++ js-20240215/script.js
@@ -25,3 +25,30 @@ console.log(2 + 2);
 
 console.log("3.");
 console.log(25 / 5 == 5 && 50 / 10 == 5);
+
+console.log("Danielles Code");
+
+console.log("Hello World!");
+console.log("D" + "a" + "n" + "i" + "e" + "l" + "l" + "e");
+// Error! Make it work
+console.log("Hallo");
+// Error! Make it work
+console.log("That doesn't work");
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 === 52);
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+// Comparison of two conditions
+console.log((15 / 8 && 50 / 10) == 5);
+
+console.log("chat GPT");
+console.log(
+  (function () {
+    const result = 5;
+    return 25 / 5 === result && 50 / 10 === result;
+  })()
+);
