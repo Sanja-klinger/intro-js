142961f22a4a3677c0425114fd241b45c59b00e4
Sanja Klinger <sladjana.klinger@gmail.com>
2024-02-22 16:42:58 +0100

    feat: types the task

---
+++ js-20240222/script.js
@@ -0,0 +1,16 @@
+function sayHello() {
+  console.log("Hello!");
+  return "Hello!";
+}
+console.log(sayHello());
+
+function add(a, b, c) {
+  return a + b + c;
+}
+
+console.log(add(7, 3, 2));
+
+function popij(piće) {
+  return "Ich trinke gerne " + piće;
+}
+console.log(popij("Wasser"));
