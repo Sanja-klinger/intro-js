4856a329e0c05b15152c2e86818cf0ae9849ecf8
Sanja Klinger <sladjana.klinger@gmail.com>
2024-03-14 10:46:17 +0100

    chore: write code for the kata

---
+++ 20240313-katas/script.js
@@ -0,0 +1,21 @@
+function mergeArrays(arr1, arr2) {
+  let mergedArray = arr1.concat(arr2);
+
+  function sortNumbers(a, b) {
+    return a - b;
+  }
+
+  mergedArray.sort(sortNumbers);
+
+  let mergedSortedArrayEachElementOnce = [];
+  for (i = 0; i < mergedArray.length; i++) {
+    if (i === 0 || mergedArray[i] !== mergedArray[i - 1]) {
+      mergedSortedArrayEachElementOnce.push(mergedArray[i]);
+    }
+  }
+  return mergedSortedArrayEachElementOnce;
+}
+
+console.log(mergeArrays([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]));
+console.log(mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2]));
+console.log(mergeArrays([1, 2, 3, 4, 5], [1, 3, 5, 7, 9, 11]));
